7ff59b786f1c9a108ec009ef0ad64499343a852d
Ton Soares <everton.soares@veri.com.br>
2022-09-08 17:44:35 -0300

    fix: using storybook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Button from "../src/components/Button/Button";
+
 export default function HomeScreen() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Button>Clica aqui</Button>
     </div>
   );
 }
